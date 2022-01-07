f98212cacb2d89432436e8e7b87eecb943cf9648
Henry <HenryFBP@gmail.com>
2022-01-07 02:21:20 +0000

    detect http server

--- autohackthebox/autohackthebox.py
+++ autohackthebox/autohackthebox.py
@@ -1,6 +1,7 @@
 from typing import Optional, List, Set
 
 import nmap3
+import lxml
 from lxml import etree
 from lxml import objectify
 
@@ -12,8 +13,8 @@ nmap = nmap3.Nmap()
 class NMAPResult:
     def __init__(self, raw_xml: str):
         self.raw_xml = raw_xml
-        self.data = etree.fromstring(self.raw_xml.encode('ascii'))
-        self.objectified_data = objectify.fromstring(self.raw_xml.encode('ascii'))
+        self.data: lxml.etree.Element = etree.fromstring(self.raw_xml.encode('ascii'))
+        self.objectified_data: lxml.objectify.ObjectifiedElement = objectify.fromstring(self.raw_xml.encode('ascii'))
 
     def extractVulnFeatures(self) -> Set[BoxVulnerabilityFeature]:
         """From my results, what vulnerable features does this scan exhibit?"""
@@ -21,17 +22,32 @@ class NMAPResult:
 
     def isOnline(self) -> bool:
         """Does this nmap result say host is online?"""
-        state = self.data.xpath('//nmaprun/host[1]/status/@state')
+        state = self.data.xpath('//nmaprun/host/status/@state')
 
         if len(state) > 0:
-            return state[0] == 'up'
+            return state[0] == 'up'  # TODO: How can I return the port? XPATH backreferences?
 
         return False
 
+    def hasService(self, service: str) -> List[lxml.etree.Element]:
+        return self.data.xpath('//nmaprun/host/ports/port/service[@name="{}"]'.format(service))
+
     def hasSSH(self) -> bool:
-        return 'wow ;)'
+        foo = self.hasService('ssh')
+
+        if len(foo) > 0:
+            return True  # TODO: How can I return the port? XPATH backreferences?
+
+        return False
 
     # def
+    def hasHTTPServer(self):
+        foo = self.hasService('http')
+
+        if len(foo) > 0:
+            return True  # TODO: How can I return the port? XPATH backreferences?
+
+        return False
 
 
 class Box:
@@ -41,6 +57,9 @@ class Box:
         self.ip = ip
         self.hostname = hostname
         self.nmap_results: List[NMAPResult] = []
+
+    def last_nmap_result(self):
+        return self.nmap_results[-1]
 
     def get_ip_or_hostname(self):
         if self.ip:
@@ -70,12 +89,14 @@ class Box:
 def hackthe(box: Box) -> Box:
     box.run_nmap_scan()
 
-    if box.nmap_results[-1].isOnline():
+    if box.last_nmap_result().isOnline():
         print(f"Most recent nmap scan says box {box} is online!")
     else:
         raise ConnectionError(f"Box {box} is offline!")
 
-    # todo do stuff based off results
+    if box.last_nmap_result().hasHTTPServer():
+        print("target has an HTTP server!")
+        raise NotImplementedError("TODO: send http request and search for <form> or HTTP 302s")
 
     return box
 
